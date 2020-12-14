cdf5d10a3c495f6efd927783f89eb0f63792d158
Joey3001 <josephnero111@gmail.com>
2020-12-13 23:05:19 -0800

    Update UI

--- Bakery.Solution/Bakery/Program.cs
+++ Bakery.Solution/Bakery/Program.cs
@@ -6,9 +6,9 @@ public class Program
   {
     Console.WriteLine("");
     Console.WriteLine("Hello there! Welcome to Joe's bakery. We have the following specials: ");
-    Console.WriteLine("------------------------------------------------");
-    Console.WriteLine("Bread is 5 Dollars per Loaf. Buy 2 get 1 free!");
-    Console.WriteLine("Pastries are 2 Dollars each, or 3 for 5 Dollars!");
+    Console.WriteLine("");
+    Console.WriteLine("1. Bread is 5 Dollars per Loaf. Buy 2 get 1 free!");
+    Console.WriteLine("2. Pastries are 2 Dollars each, or 3 for 5 Dollars!");
     Console.WriteLine("");
     Console.WriteLine("Enter the number of bread loafs you'd like to purchase: ");
     int breadLoafs = int.Parse(Console.ReadLine()); 
@@ -18,12 +18,12 @@ public class Program
     PastryOrder newPastryOrder = new PastryOrder(pastries); 
     Console.WriteLine("");
     Console.WriteLine("The cost of your bread order is: ");
-    Console.WriteLine(newBreadOrder.OrderCost());
+    Console.WriteLine(newBreadOrder.OrderCost() + " Dollars");
     Console.WriteLine("The cost of your pastry order is: ");
-    Console.WriteLine(newPastryOrder.OrderCost());
+    Console.WriteLine(newPastryOrder.OrderCost() + " Dollars");
     OrderCalculator newOrderCalculator = new OrderCalculator(); 
     Console.WriteLine("");
     Console.WriteLine("The total cost of your order is: ");
-    Console.WriteLine(newOrderCalculator.TotalCost(newBreadOrder.OrderCost(), newPastryOrder.OrderCost()));
+    Console.WriteLine(newOrderCalculator.TotalCost(newBreadOrder.OrderCost(), newPastryOrder.OrderCost()) + " Dollars");
   }
 }
