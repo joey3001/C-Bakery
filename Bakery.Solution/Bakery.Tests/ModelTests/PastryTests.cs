8f9485e828f331d471169662f2e1576c8a8dd21d
Joey3001 <josephnero111@gmail.com>
2020-12-16 20:42:02 -0800

    Refactor PastryOrder Tests

--- Bakery.Solution/Bakery.Tests/ModelTests/PastryTests.cs
+++ Bakery.Solution/Bakery.Tests/ModelTests/PastryTests.cs
@@ -9,29 +9,59 @@ namespace Bakery.Tests
     [TestMethod]
     public void PastryOrder_CreateInstanceOfPastryOrder_PastryOrder()
     {
-      PastryOrder newPastryOrder = new PastryOrder(2);
+      int numberOfPastries = 4; 
+
+      PastryOrder newPastryOrder = new PastryOrder(numberOfPastries);
+
       Assert.AreEqual(typeof(PastryOrder), newPastryOrder.GetType());
     }
 
     [TestMethod]
-    public void PastryOrder_ProvideInputForNumberOfPastries_2()
+    public void PastryOrder_ProvideInputForNumberOfPastriesToPurchaseInPastryOrder_int()
     {
-      PastryOrder newPastryOrder = new PastryOrder(2);
-      Assert.AreEqual(2, newPastryOrder.NumberOfPastries);
+      int numberOfPastries = 4; 
+      int intendedNumberOfPastries = 4; 
+
+      PastryOrder newPastryOrder = new PastryOrder(numberOfPastries);
+      numberOfPastries = newPastryOrder.NumberOfPastries;
+
+      Assert.AreEqual(intendedNumberOfPastries, numberOfPastries);
     }
 
     [TestMethod]
-    public void PastryOrder_SetCostPerPastryTo2_2()
+    public void PastryOrder_SetCostPerPastryTo2_int()
     {
-      PastryOrder newPastryOrder = new PastryOrder(2);
-      Assert.AreEqual(2, newPastryOrder.CostPerPastry);
+      int numberOfPastries = 4; 
+      int intendedCostPerPastry = 2; 
+
+      PastryOrder newPastryOrder = new PastryOrder(numberOfPastries);
+      int costPerPastry = newPastryOrder.CostPerPastry; 
+
+      Assert.AreEqual(intendedCostPerPastry, costPerPastry);
     }
 
     [TestMethod]
-    public void OrderCost_CalculateOrderCostsForPastries_15()
+    public void OrderCost_CalculateCostOfAmountOfPastriesWithoutDiscount_int()
     {
-      PastryOrder newPastryOrder = new PastryOrder(9);
-      Assert.AreEqual(15, newPastryOrder.OrderCost());
+      int numberOfPastries = 2;
+      int intendedOrderCost = 4; 
+
+      PastryOrder newPastryOrder = new PastryOrder(numberOfPastries);
+      int pastryOrderCost = newPastryOrder.OrderCost(); 
+
+      Assert.AreEqual(intendedOrderCost, pastryOrderCost);
+    }
+
+   [TestMethod]
+    public void OrderCost_CalculateCostOfAmountOfPastriesWithDiscount_int()
+    {
+      int numberOfPastries = 3;
+      int intendedOrderCost = 5; 
+
+      PastryOrder newPastryOrder = new PastryOrder(numberOfPastries);
+      int pastryOrderCost = newPastryOrder.OrderCost(); 
+
+      Assert.AreEqual(intendedOrderCost, pastryOrderCost);
     }
   }
 }
